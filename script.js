0f74c36a7209f47dccc9cc8891854d8fabd6fe42
shafaat Hussain <shafaath508@gmail.com>
2024-05-05 16:09:30 +0530

    hello

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index_copy.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(3000, () => {
